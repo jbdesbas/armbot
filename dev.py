4da943a54016c29c0c6944bb5f6da77fa54b4f5e
Jean-Baptiste Desbas <jb@desbas.fr>
2023-08-28 16:04:37 +0200

    motors config + test sequence

--- dev.py
+++ dev.py
@@ -13,6 +13,11 @@ pca.frequency = 60
 MIN = 6000
 MAX = 9500
 
+motors_config = {
+    'base' : {'channel' : 0, 'min_position' : 6000, 'max_position': 9500},
+    'shoulder' : {'channel' : 1, 'min_position' : 5500, 'max_position': 10000},
+    'elbow' : {'channel' : 2, 'min_position' : 3000, 'max_position': 8000}
+}
 
 def range_2ways(start,stop):
     return range(start, stop, 1 if start < stop else -1)
@@ -27,34 +32,40 @@ class stepMotor():
     
     def initMotor(self):
         """centrale position"""
-        print((self.min_position+self.max_position)/2)
-        sleep(1)
-        print(0)
+        pca.channels[self.channel].duty_cycle = int((self.min_position+self.max_position)/2)
+        print('Init', self.channel)
+        sleep(3) #Pour que les moteurs s'initient 1 par 1
+        pca.channels[self.channel].duty_cycle = 0
         self.current_position = (self.min_position+self.max_position)/2
        
     async def goTo(self,target, speed = 1):
-        print('target : ',target)
+        print('{} go to {}'.format(self.channel, target) )
         try:
             for p in range_2ways(int(self.current_position), target): 
                 #print(p)
                 pca.channels[self.channel].duty_cycle = p
                 await asyncio.sleep(0.001)
-            print('Mouvement terminé {}, déplacement de {}'.format(self.channel, self.current_position - target) )
         except asyncio.CancelledError:
-            print('Mouvement annulé en route !') # Si le mouvement est arrếté
+            print('Mouvement annulé en route !', self.channel) # Si le mouvement est arrếté
         finally:
-            self.current_position = p
             pca.channels[self.channel].duty_cycle = 0
-            print(0) # Si le mouvement est annulé avant la fin ou terminé, on coupe le moteur.
+            print('{} is now {} ({})'.format(self.channel,  p, self.current_position - target) )
+            self.current_position = p
 
 
-base = stepMotor(0)
-epaule = stepMotor(1)
+base = stepMotor(**motors_config['base'])
+shoulder = stepMotor(**motors_config['shoulder'])
+elbow = stepMotor(**motors_config['elbow'])
 
 async def main():
     while True :
-        task = asyncio.create_task(base.goTo(6200)) # Annuler toute autre tache qui concerne ce moteur !
-        task2 = asyncio.create_task(epaule.goTo(8500))
+        asyncio.create_task(base.goTo(6200)) # Annuler toute autre tache qui concerne ce moteur !
+        asyncio.create_task(shoulder.goTo(7000))
+        asyncio.create_task(elbow.goTo(7000))
+        await asyncio.sleep(15)
+        asyncio.create_task(base.goTo(9000)) # Annuler toute autre tache qui concerne ce moteur !
+        asyncio.create_task(shoulder.goTo(9000))
+        asyncio.create_task(elbow.goTo(4000))
         await asyncio.sleep(15)
 
 
