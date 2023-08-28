5e950db617f3f25134636c413ddba26e2a9051df
Jean-Baptiste Desbas <jb@desbas.fr>
2023-08-28 22:44:54 +0200

    task lock

--- dev.py
+++ dev.py
@@ -29,17 +29,19 @@ class stepMotor():
         self.current_position = None
         self.channel = channel
         self.initMotor()
+        self.lock = asyncio.Lock()
     
     def initMotor(self):
         """centrale position"""
         pca.channels[self.channel].duty_cycle = int((self.min_position+self.max_position)/2)
         print('Init', self.channel)
-        sleep(3) #Pour que les moteurs s'initient 1 par 1
+        sleep(1.5) #Pour que les moteurs s'initient 1 par 1
         pca.channels[self.channel].duty_cycle = 0
         self.current_position = (self.min_position+self.max_position)/2
        
     async def goTo(self,target, speed = 1):
         print('{} go to {}'.format(self.channel, target) )
+        await self.lock.acquire()
         try:
             for p in range_2ways(int(self.current_position), target): 
                 #print(p)
@@ -51,6 +53,7 @@ class stepMotor():
             pca.channels[self.channel].duty_cycle = 0
             print('{} is now {} ({})'.format(self.channel,  p, self.current_position - target) )
             self.current_position = p
+            self.lock.release()
 
 
 base = stepMotor(**motors_config['base'])
@@ -58,18 +61,19 @@ shoulder = stepMotor(**motors_config['shoulder'])
 elbow = stepMotor(**motors_config['elbow'])
 
 async def main():
-    while True :
-        asyncio.create_task(base.goTo(6200)) # Annuler toute autre tache qui concerne ce moteur !
-        asyncio.create_task(shoulder.goTo(7000))
-        asyncio.create_task(elbow.goTo(7000))
-        await asyncio.sleep(5)
-        asyncio.create_task(base.goTo(9000)) # Annuler toute autre tache qui concerne ce moteur !
-        asyncio.create_task(shoulder.goTo(9000))
-        asyncio.create_task(elbow.goTo(4000))
-        await asyncio.sleep(5)
+    tasks=[]
+    for x in range(1,3):
+        tasks.append(asyncio.create_task(base.goTo(6200))) # TODO Grouper par "mouvement" (combinaison de déplacement des différents moteurs, ne passer au mouvement suivant que quand tous les moteurs ont fini
+        tasks.append(asyncio.create_task(shoulder.goTo(7000)))
+        tasks.append(asyncio.create_task(elbow.goTo(7000)))
+        
+        tasks.append(asyncio.create_task(base.goTo(9000))) 
+        tasks.append(asyncio.create_task(shoulder.goTo(9000)))
+        tasks.append(asyncio.create_task(elbow.goTo(4000)))
+        await asyncio.gather(*tasks)
 
 
-asyncio.run(main())
 
+asyncio.get_event_loop().run_until_complete(main())
 
 
